8a5e644a54fab7662057ca3efaca095892678179
Yog2705 <yogiiyogsyy@gmail.com>
2026-09-12 19:37:57 +0530

    Fix production API configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  define: {
+    "import.meta.env.VITE_API_URL": JSON.stringify(
+      process.env.API_URL
+    ),
+
+    "import.meta.env.VITE_SOCKET_URL": JSON.stringify(
+      process.env.SOCKET_URL
+    ),
+  },
+});
